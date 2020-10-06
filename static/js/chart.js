3c6cc34531df8a2f88264e183d6c0d652aeb989e
Adam <silvertitaniumx@gmail.com>
2020-10-05 23:10:36 -0700

    deliverable 2

--- static/js/chart.js
+++ static/js/chart.js
@@ -90,5 +90,30 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_id,
+      y: sample_value,
+      text: otu_label,
+      mode: 'markers',
+      marker: {
+        size: sample_value,
+        color: otu_id,
+        colorscale: 'Earth'
+      }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {
+        title: 'OTU ID',
+      },
+      showlegend: false
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   });
 }
